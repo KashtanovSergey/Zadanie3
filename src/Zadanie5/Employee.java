bf7eb80c31577db75689082d116b564556cab92a
Sergey <kashtanov_sv@bk.ru>
2022-06-07 11:52:17 +0300

    homework to lesson-3

---
+++ src/Zadanie5/Employee.java
@@ -0,0 +1,21 @@
+package Zadanie5;
+
+public class main {
+    public static void main(Strings[] args){
+        String FIO = "";
+        String post = "";
+        String e-mail = "";
+        int t-phone = 0;
+        double salary = 0;
+        int age = 0;
+        Vol employee1 = new employee();
+        Vol employee2 = new employeeVol();
+        Vol employee3 = new employee();
+        Object
+    }
+    public void volumeEmploy(String name){
+
+    }
+
+
+}
